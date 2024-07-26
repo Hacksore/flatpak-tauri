915d61a3bc0d21c6e5cc0244da5e6bd8511c6e77
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-07-26 10:12:23 -0500

    Interactive mode?

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # TODO: this should not be interactive and should just do all the stuffz
-# install all the things we need for building flatpak & tauri on ubuntu 
+export DEBIAN_FRONTEND=noninteractive
 
+# install all the things we need for building flatpak & tauri on ubuntu 
 curl -fsSL https://deb.nodesource.com/setup_20.x | sudo bash -
-curl -fsSL https://get.pnpm.io/install.sh | sh -
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl -fsSL https://get.pnpm.io/install.sh | sh -s -- -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 sudo apt-get update
 sudo apt-get install -y libgtk-3-dev libwebkit2gtk-4.0-dev libappindicator3-dev librsvg2-dev patchelf flatpak flatpak-builder build-essential
