0da497f0fdc7a4402afa3996fdd38b874c7a897e
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-07-26 09:17:50 -0500

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# install all the things we need for building flatpak & tauri on ubuntu 
+
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo bash -
+curl -fsSL https://get.pnpm.io/install.sh | sh -
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+sudo apt-get update
+sudo apt-get install -y libgtk-3-dev libwebkit2gtk-4.0-dev libappindicator3-dev librsvg2-dev patchelf flatpak flatpak-builder
