986884f7c29fdd2d0ef41070e00f6d55f818919b
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-07-26 09:48:30 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,3 +7,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 sudo apt-get update
 sudo apt-get install -y libgtk-3-dev libwebkit2gtk-4.0-dev libappindicator3-dev librsvg2-dev patchelf flatpak flatpak-builder build-essential
+
+# setup flatpak
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak --user install flathub -y --noninteractive org.gnome.Sdk/x86_64/46
